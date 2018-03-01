7adce00d84011b1b98d9e78eb393891b91305a7f
Jeff Kingyens <jeff.kingyens@gmail.com>
2018-02-28 20:02:22 -0800

    parammaterize email

--- renew.sh
+++ renew.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-certbot --email jeff.kingyens@gmail.com --agree-tos --manual-public-ip-logging-ok -n -d ${RENEW_DOMAIN} --manual-auth-hook /root/auth.sh --manual --preferred-challenges dns certonly
+certbot --email ${RENEW_EMAIL} --agree-tos --manual-public-ip-logging-ok -n -d ${RENEW_DOMAIN} --manual-auth-hook /root/auth.sh --manual --preferred-challenges dns certonly
 node cleanup.js
